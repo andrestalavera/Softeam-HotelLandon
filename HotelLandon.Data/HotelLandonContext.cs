262016ac092b4e6896aca646bac14f805af8eb67
Andres Talavera <andres.talavera@live.fr>
2020-11-18 18:40:44 +0100

    First code

---
+++ HotelLandon.Data/HotelLandonContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelLandon.Data
+{
+    public class HotelLandonContext : DbContext
+    {
+    }
+}
---
+++ HotelLandon.Models/Customers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HotelLandon.Models
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+    }
+
+    public class Customers : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class Reservation : EntityBase
+    {
+        public DateTime Start       { get; set; }
+        public DateTime End         { get; set; }
+        public Room     Room        { get; set; }
+        public Customer Customer    { get; set; }
+    }
+
+    public class Room : EntityBase
+    {
+        
+    }
+}
