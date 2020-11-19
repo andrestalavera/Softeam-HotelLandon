8e439e931c39e52a0b56e20bceb6743bac359814
Andres Talavera <andres.talavera@live.fr>
2020-11-19 19:32:01 +0100

    Update code

--- HotelLandon.Data/HotelLandonContext.cs
+++ HotelLandon.Data/HotelLandonContext.cs
@@ -8,5 +8,11 @@ namespace HotelLandon.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelLandon;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 }
