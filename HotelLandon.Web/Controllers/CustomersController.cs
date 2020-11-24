4a8d97983c977bc8c6aafd1002ebe510260d0aca
Andres Talavera <andres.talavera@live.fr>
2020-11-24 19:34:14 +0100

    Add logs and Export button (export to Storage)

--- HotelLandon.Web/Controllers/CustomersController.cs
+++ HotelLandon.Web/Controllers/CustomersController.cs
@@ -1,9 +1,15 @@
-﻿using HotelLandon.Data;
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using HotelLandon.Data;
 using HotelLandon.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelLandon.Web.Controllers
@@ -22,23 +28,29 @@ namespace HotelLandon.Web.Controllers
         // GET: Customers
         public async Task<IActionResult> Index()
         {
+            _logger.LogInformation("Welcome to Customers page");
             return View(await _context.Customers.ToListAsync());
         }
 
         // GET: Customers/Details/5
         public async Task<IActionResult> Details(int? id)
         {
+            _logger.LogInformation($"Welcome to Customer page, id {id}");
             if (id == null)
             {
+                _logger.LogError("Id is null");
                 return NotFound();
             }
 
             var customer = await _context.Customers
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (customer == null)
             {
+                _logger.LogError($"Customer not found");
                 return NotFound();
             }
+            _logger.LogInformation($"{customer.FirstName} {customer.LastName} found. Returning to details");
 
             return View(customer);
         }
@@ -142,6 +154,38 @@ namespace HotelLandon.Web.Controllers
             var customer = await _context.Customers.FindAsync(id);
             _context.Customers.Remove(customer);
             await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Export()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            foreach (var customer in customers)
+            {
+                // Newtonsoft.Json
+                var json = JsonConvert.SerializeObject(customer);
+                // send to Azure Storage
+
+                var config = new
+                {
+                    AccountName = "softeamandrestalavera001",
+                    ImageContainer = "demo001",
+                    AccountKey = "T3ZmtK4kCoUUKWdiMTb2zYSHaWifC8Ikd4lqAFr487YpFxNLxCfTl5GCaLPJP8pIlNGOtcOcUnkx87JwAyhwyw=="
+                };
+
+                byte[] byteArray = Encoding.ASCII.GetBytes(json);
+                using MemoryStream stream = new MemoryStream(byteArray);
+                var blobUri = new Uri("https://" +
+                              config.AccountName +
+                              ".blob.core.windows.net/" +
+                              config.ImageContainer +
+                              "/" + Guid.NewGuid());
+
+                var storageCredentials = new StorageSharedKeyCredential(config.AccountName, config.AccountKey);
+                var blobClient = new BlobClient(blobUri, storageCredentials);
+
+                await blobClient.UploadAsync(stream, overwrite: true);
+            }
             return RedirectToAction(nameof(Index));
         }
 
